03715dd063747df22fcac5ab2e0ae06468a3caad
hya-th <satellitee038@gmail.com>
2025-12-10 17:51:19 +0900

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,24 @@
+$(document).ready(function () {
+  const starCount = 500;
+
+  for (let i = 0; i < starCount; i++) {
+    const $star = $("<div class='star'></div>");
+
+    const top = Math.random() * 100 + "%";
+    const left = Math.random() * 100 + "%";
+
+    const size = Math.random() * 2 + 3 + "px";
+
+    const delay = (Math.random() * 2).toFixed(2) + "s";
+
+    $star.css({
+      top: top,
+      left: left,
+      width: size,
+      height: size,
+      animationDelay: delay,
+    });
+
+    $(".stars-container").append($star);
+  }
+});
